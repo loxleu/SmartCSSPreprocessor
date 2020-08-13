ed2fc81e94c9d1f3ad0e18c2473fb5f95db3a2c2
Селезень <53906410+loxleu@users.noreply.github.com>
2020-08-13 11:38:20 +0300

    Started adding the code to add the program to the %PATH%

--- scp-core/Program.cs
+++ scp-core/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 
 namespace scp_core
 {
-    class Program
+    class Mainclass
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main entry point of the program
+        /// </summary>
+        public static void Main()
         {
+            string path = Environment.GetEnvironmentVariable("PATH") + Environment.CurrentDirectory;
+            string[] args = Environment.GetCommandLineArgs();
+            Environment.SetEnvironmentVariable("PATH", path);
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
